e4b38e409b5bf54e811d929300ce2de3a9efb077
sistemkoordinat <faridahmyun@gmail.com>
2020-08-04 15:34:31 -0700

    fm

--- pengenalan.js
+++ pengenalan.js
@@ -1,8 +1,6 @@
-function myFunction() {
-    var x = document.getElementById("myDIV");
-    if (x.style.display == "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
-  }
+var x = document.getElementById("petunjuk");
+var xx= document.querySelector('.petunjuk');
+
+xx.addEventListener("click",function(){
+  x.classList.toggle("hilang");
+})
