84a2675805017632310ac38dc1c6be77a5d8d0a5
sistemkoordinat <faridahmyun@gmail.com>
2020-09-16 11:59:44 -0700

    fm

---
+++ nilaievaluasi/script.js
@@ -0,0 +1,132 @@
+// menguhubungkan ke firebase
+var firebaseConfig = {
+    apiKey: "AIzaSyBNS_wz_CMntH55l8Rb0lVfYmo66zRfkO4",
+    authDomain: "sistemkoordinat.firebaseapp.com",
+    databaseURL: "https://sistemkoordinat.firebaseio.com",
+    projectId: "sistemkoordinat",
+    storageBucket: "sistemkoordinat.appspot.com",
+    messagingSenderId: "322719063336",
+    appId: "1:322719063336:web:6702959c04f1874f85841d"
+  };
+
+  firebase.initializeApp(firebaseConfig);
+
+
+window.load = function () {
+
+
+}
+
+let kmplanId = [];
+
+
+let kuisnya = document.getElementById('kuis');
+
+
+let tmp = document.querySelector('.disini');
+tmp.innerHTML = "";
+kelasnya = document.getElementById('kelas');
+sekolah = document.getElementById('sekolah');
+let kelasfix = '';
+let sekolahfix = '';
+
+let cek11 = 0;
+kuisfix = kuisnya.value;
+
+
+function readlah() {
+    kuisfix = kuisnya.value;
+    console.log(kuisfix);
+    var task = firebase.database().ref(kuisfix);
+
+    tmp.innerHTML = "";
+    if (kelasnya.value == "1") {
+        kelasfix = "8A";
+    } else if (kelasnya.value == "2") {
+        kelasfix = "8B";
+    } 
+
+    if (sekolah.value == "1") {
+        sekolahfix = "SMP Negeri 13 Banjarmasin";
+    } 
+
+
+    let jwbfixx = [];
+
+    let jwb1 = ["c", "a", "c", "b", "a", "a", "d", "c", "b", "d", "d", "d", "b", "b", "b", "b", "c", "c", "b", "d" ];
+    if (kuisfix == "evaluasi/") {
+        let hhh = `<tr><td class="nah" colspan="2">Kunci Jawaban </td>`;
+        for (let i = 0; i < jwb1.length; i++) {
+            hhh += `<td class = "nah"> ${jwb1[i]}</td>`;
+        }
+        hhh += `</tr>`;
+        tmp.innerHTML += hhh;
+        jwbfixx = jwb1;
+    }
+
+    
+
+    if ((sekolahfix != '') && (kelasfix != '')) {
+        task.orderByChild("nama").on("child_added", function (data) {
+            // task.on("child_added", function (data) {
+            var taskvalue = data.val();
+
+
+
+
+            if ((sekolahfix == taskvalue.sekolah) && (kelasfix == taskvalue.kelas)) {
+                let mm = '';
+                mm = `<tr><td >${taskvalue.nama}</td><td >${taskvalue.nilai}</td>`;
+                // console.log(taskvalue.jawabannya[0]);
+
+
+                for (let i = 0; i < jwbfixx.length; i++) {
+                    if (taskvalue.jawabannya[i] == jwbfixx[i]) {
+                        mm += `<td class="benar">${taskvalue.jawabannya[i]}</td>`;
+                    } else {
+                        mm += `<td class="salah">${taskvalue.jawabannya[i]}</td>`;
+                    }
+                }
+                mm += `</tr>`;
+                tmp.innerHTML += mm;
+            }
+
+        });
+
+
+
+
+
+    } else {
+        alert('Tentukan filter pencarian');
+    }
+
+
+
+}
+
+window.onload = function () {
+    kelasnya = document.getElementById('kelas');
+    sekolah = document.getElementById('sekolah');
+    kuis = document.getElementById('kuis');
+    kelasnya.value = value = "0";
+    sekolah.value = value = "0";
+    kuis.value = value = "evaluasi/";
+}
+
+
+
+
+// download data
+
+let download = document.querySelector('.download');
+download.addEventListener('click', function () {
+    var data_type = 'data:application/vnd.ms-excel';
+    var table_div = document.getElementById('table_wrapper');
+    var table_html = table_div.outerHTML.replace(/ /g, '%20');
+
+    var a = document.createElement('a');
+    a.href = data_type + ', ' + table_html;
+    a.download = 'exported_table_' + Math.floor((Math.random() * 9999999) + 1000000) + '.xls';
+    a.click();
+})
